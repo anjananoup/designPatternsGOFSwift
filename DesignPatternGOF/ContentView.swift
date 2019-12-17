e89a5df690dae55265cadceb58086c2a386ea2be
Anjan Kumar Majumder <anjan.m@samsung.com>
2019-12-17 11:58:30 +0600

    Initial Commit

---
+++ DesignPatternGOF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignPatternGOF
+//
+//  Created by Anjan on 12/17/19.
+//  Copyright © 2019 Anjan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
